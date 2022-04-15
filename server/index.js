af90ae398b846b98f4fa51a67bb631bbeabc2a0f
Jesus Rosales <jrosales6213@gmail.com>
2022-04-15 08:23:17 -0700

    update

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
+require('dotenv').config()
 import postRoutes from './routes/posts.js';
 
 const app = express();
