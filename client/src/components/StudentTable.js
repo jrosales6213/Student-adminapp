666271d8ad66741aa8f0b1d9439a9b30fe4bbf11
Jesus Rosales <jrosales6213@gmail.com>
2022-04-22 06:29:07 -0700

    student form completed

--- client/src/components/StudentTable.js
+++ client/src/components/StudentTable.js
@@ -18,28 +18,39 @@ import CIcon from '@coreui/icons-react'
 import { cilPencil, cilDelete, cilNotes } from '@coreui/icons'
 import { useSelector } from 'react-redux'
 
-const StudentRow = ({ posts }) => {
+function StudentRow({ post }) {
+  const handleclick = () => {
+    console.log('you clikced a button')
+  }
   return (
-    <CTableRow>
-      <CTableHeaderCell scope="row">{posts.firstname}</CTableHeaderCell>
-
-      <CTableDataCell>{posts.lastname}</CTableDataCell>
-      <CTableDataCell>{posts.mobile}</CTableDataCell>
-
-      <CTableDataCell className="d-flex justify-content-center">
-        <CCol>
-          <CIcon icon={cilPencil} size={'lg'} className="me-2 pr-3 pl-3s"></CIcon>
-        </CCol>
-        <CCol>
-          <CIcon icon={cilDelete} size={'lg'} className="me-2 pr-3 pl-3s"></CIcon>
-        </CCol>
-        <CCol>
-          <CIcon icon={cilNotes} size={'lg'} className="me-2 pr-3 pl-3s"></CIcon>
-        </CCol>
-      </CTableDataCell>
-    </CTableRow>
+    <>
+      {post.map((item) => (
+        <CTableRow key={item._id}>
+          <CTableHeaderCell>{item.firstname}</CTableHeaderCell>
+          <CTableDataCell>{item.lastname}</CTableDataCell>
+          <CTableDataCell>{item.mobile}</CTableDataCell>
+          <CTableDataCell className="d-flex justify-content-center">
+            <CCol>
+              <CIcon
+                icon={cilPencil}
+                size={'lg'}
+                className="me-2 pr-3 pl-3s"
+                onClick={handleclick}
+              ></CIcon>
+            </CCol>
+            <CCol>
+              <CIcon icon={cilDelete} size={'lg'} className="me-2 pr-3 pl-3s"></CIcon>
+            </CCol>
+            <CCol>
+              <CIcon icon={cilNotes} size={'lg'} className="me-2 pr-3 pl-3s"></CIcon>
+            </CCol>
+          </CTableDataCell>
+        </CTableRow>
+      ))}
+    </>
   )
 }
+
 const StudentTable = () => {
   const posts = useSelector((state) => state.posts)
   return (
@@ -51,7 +62,6 @@ const StudentTable = () => {
               <strong>Students</strong>
             </CCardHeader>
             <CCardBody>
-              {/* <DocsExample href="components/table#variants"> */}
               <CTable>
                 <CTableHead>
                   <CTableRow>
@@ -62,67 +72,9 @@ const StudentTable = () => {
                   </CTableRow>
                 </CTableHead>
                 <CTableBody>
-                  <StudentRow posts={posts} />
-                  {/* <CTableRow>
-                    <CTableHeaderCell scope="row">{posts.firstname}</CTableHeaderCell>
-                    <CTableDataCell>{posts.lastname}</CTableDataCell>
-                    <CTableDataCell>143543</CTableDataCell>
-                    <CTableDataCell className="d-flex justify-content-center">
-                      <CCol>
-                        <CIcon icon={cilPencil} size={'lg'} className="me-2 pr-3 pl-3s"></CIcon>
-                      </CCol>
-                      <CCol>
-                        <CIcon icon={cilDelete} size={'lg'} className="me-2 pr-3 pl-3s"></CIcon>
-                      </CCol>
-                      <CCol>
-                        <CIcon icon={cilNotes} size={'lg'} className="me-2 pr-3 pl-3s"></CIcon>
-                      </CCol>
-                    </CTableDataCell>
-                  </CTableRow> */}
-
-                  {/* <CTableRow color="primary">
-                    <CTableHeaderCell scope="row">Primary</CTableHeaderCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                  </CTableRow>
-                  <CTableRow color="secondary">
-                    <CTableHeaderCell scope="row">Secondary</CTableHeaderCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                  </CTableRow>
-                  <CTableRow color="success">
-                    <CTableHeaderCell scope="row">Success</CTableHeaderCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                  </CTableRow>
-                  <CTableRow color="danger">
-                    <CTableHeaderCell scope="row">Danger</CTableHeaderCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                  </CTableRow>
-                  <CTableRow color="warning">
-                    <CTableHeaderCell scope="row">Warning</CTableHeaderCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                  </CTableRow>
-                  <CTableRow color="info">
-                    <CTableHeaderCell scope="row">Info</CTableHeaderCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                  </CTableRow>
-                  <CTableRow color="light">
-                    <CTableHeaderCell scope="row">Light</CTableHeaderCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                  </CTableRow>
-                  <CTableRow color="dark">
-                    <CTableHeaderCell scope="row">Dark</CTableHeaderCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                    <CTableDataCell>Cell</CTableDataCell>
-                  </CTableRow> */}
+                  <StudentRow post={posts} />
                 </CTableBody>
               </CTable>
-              {/* </DocsExample> */}
             </CCardBody>
           </CCard>
         </CCol>
@@ -135,8 +87,7 @@ export default StudentTable
 
 StudentTable.propTypes = {
   setCurrentId: PropTypes.number,
-  firstname: PropTypes.string,
-  lastname: PropTypes.string,
-  mobile: PropTypes.string,
-  posts: PropTypes.object,
+}
+StudentRow.propTypes = {
+  post: PropTypes.object,
 }
